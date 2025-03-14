fbedd4b2bbb4617a045374751e6b79fb3bcc9706
Erick ribeiro <ribeiroerick2024@gmail.com>
2025-03-14 13:51:22 -0300

    commit teste

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h2 className="text-4xl font-bold text-blue-600 mb-4">Página Inicial</h2>
       <p className="text-lg text-gray-700">Bem-vindo à Wolfmarcenaria, seu espaço de criação!</p>
+      <p className="text-lg text-gray-700">Bem-vindo à Wolfmarcenaria, seu espaço de criação!</p>
     </div>
   );
 };
