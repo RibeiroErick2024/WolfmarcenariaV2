2b9b94796b694162df3278a9f271dc85c00ef46f
Erick ribeiro <ribeiroerick2024@gmail.com>
2025-03-14 13:47:57 -0300

    commit teste

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';  // Importe o arquivo de estilo aqui
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
